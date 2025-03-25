659108ef07dddf3fa3922906d47116758e42689e
KRISHIKA <krishika1400@gmail.com>
2025-03-25 22:07:05 +0530

    ADVANCED PATTERNS [ HOLLOW RECTANGLE ]  ( DAY 1 )

---
+++ hollowrectangle.java
@@ -0,0 +1,22 @@
+
+
+public class hollowrectangle {
+    public static void Hollowrectangle(int totRows, int totCols){
+        //OUTER LOOP
+        for (int i = 1;i <= totRows;i++) {
+            for(int j = 1;j <= totCols;j++){
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+        public static void main(String[] args) {
+        Hollowrectangle(4,12);
+    }
+}
+
