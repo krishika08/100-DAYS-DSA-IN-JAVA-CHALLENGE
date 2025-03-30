5af9342bbc0aa58926e7639ca44fae9c8519c032
KRISHIKA <krishika1400@gmail.com>
2025-03-30 23:16:47 +0530

    Update FloydsTriangle.java

--- FloydsTriangle.java
+++ FloydsTriangle.java
@@ -1,7 +1,9 @@
 public class FloydsTriangle {
     public static void Floyds_Triangle(int n){
         int counter =1;
+        // OUTER LOOP
         for(int i = 1;i <= n;i++){
+            //  INNER LOOP
             for(int j =1;j <= i;j++){
                 System.out.print(counter+" ");
                 counter++;
