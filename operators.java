f906cb3bb77132851da70001f604fac43df23da9
KRISHIKA <krishika1400@gmail.com>
2025-05-05 23:17:41 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,73 @@
+public class operators {
+    public static void main(String[] args) {
+        //RELATIONAL OPERATORS
+      //  int a = 10;
+        //int b = 10;
+        //System.out.print(a==b);
+
+        // int a = 10;
+        // int b = 10;
+        // System.out.print(a!=b);
+
+        // int a = 10;
+        // int b = 11;
+        // System.out.print(b>a);
+
+    //     int a = 10;
+    //     int b = 11;
+    //     System.out.print(b<a);
+    
+    //logical"AND" OPERATOR
+        // int a = 10;
+        // int b = 11;
+        // int c = 7;
+        // System.out.print((b>a)  &&  (a<c) );
+
+        //logical"OR" OPERATOR
+        // int a = 10;
+        //  int b = 11;
+        //  int c = 7;
+        // System.out.print((b>a)  ||  (a<c) );
+
+        //logical"NOT" OPERATOR
+      //  int a = 10;
+    //     int b = 11;
+    //     System.out.print(!(b<a));
+
+    //ASSIGNMENT OPERATORS
+
+    // +=
+
+    //int A = 10;
+   // A = A + 10;
+   // System.out.println(A);
+   // A += 10;
+  //  System.out.println(A);
+        
+  //-=
+        // int b = 10;
+        // //b = b- 10;
+        // b -= 10;
+        // System.out.println(b);
+
+
+        // *=
+        //  int b = 10;
+        // //b = b* 10;
+        //  b *= 10;
+        //  System.out.println(b);
+
+        //  /=
+        // int b = 10;
+        // //b = b/10;
+        //  b /= 10;
+        //  System.out.println(b);
+
+
+        // %=
+        int b = 10;
+        //b = b%10;
+         b %= 10;
+         System.out.println(b);
+}
+}
